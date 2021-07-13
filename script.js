ae629b07ccd4ee0caa64803d3680019228ff7b1e
Maxim Fesenko <mmfesenko@gmail.com>
2021-07-13 18:28:35 +0300

    do all tasks

--- script.js
+++ script.js
@@ -9,14 +9,10 @@ let figure = document.querySelector('.figure');
 let button = document.querySelector('button');
 let selectElement = document.querySelector('.figStyle');
 
-// selectElement.addEventListener('change', (event) => {
-//     if (selectElement === 'square') {
-//         figure.classList.add('square');
-//     } else if (selectElement === 'triangle') {
-//         figure.classList.add('triangle');
-//     }
-//   figure.textContent = `Your figure is ${event.target.value}`;
-// });
+selectElement.addEventListener('change', (event) => {
+    figure.setAttribute('class', 'figure ' + event.target.value);
+  console.log(event.target.value);
+});
 
 button.onclick = function colorChoice() {
     let shapeColor = document.querySelector('input').value;
